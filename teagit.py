f9800da622447cf9b08af009a5b46a2bd8160878
Andy Elvey <andy@obsidian.(none)>
2009-05-17 13:49:39 +1200

    First commit of teagit. Share and enjoy.......;)

---
+++ teagit.py
@@ -0,0 +1,43 @@
+
+
+#  A simple program to create a thing that 
+#  is almost (but not quite) entirely unlike a Git repo. 
+
+#  If Git were a drink, and a Nutri-Matic Drink 
+#  Synthesizer attempted to make it, it would probably 
+#  create something like this....   
+
+#  Teagit - "Almost (but not quite) entirely unlike Git". 
+
+#  Author:  mooseman 
+#  This code is released to the public domain. 
+#  "Share and enjoy"...... ;)  
+
+
+#  A class to create the repo-thingy (as the Captain would say...)   
+
+class repo():
+   def setname(self, name, label):
+      self.name = name 
+	  self.label = label 
+	  
+   def init():
+      data = {} 	  
+	  
+   def sha1hash(pth): 
+      f=open(pth,'rb').read()
+      sh=hashlib.sha1(f).hexdigest()
+	  return sh 	  
+	  
+   def dirname(sh): 	  
+	  dirname = sh[0:2] 
+	  print "dirname is " + dirname 
+	  return dirname 
+		  
+   def adddata(pth):	  
+      
+   
+   
+
+ 
+
