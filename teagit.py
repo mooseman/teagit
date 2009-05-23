26870058e603ef22d4aca9985e9dd8b6f9ad1772
Andy Elvey <andy@obsidian.(none)>
2009-05-23 12:46:17 +1200

    More tidying-up of the code.

--- teagit.py
+++ teagit.py
@@ -18,35 +18,39 @@ import hashlib, os
 
 #  A class to create the repo-thingy (as the Captain would say...)   
 class repo():
-   # Name of the "repo"   	
-   def setname(self, name):	   
-      self.name = name 
+   	  
+   # Initialise the "repo"
+   # The list stores the data after the add.  	  
+   # The dict stores the data when a commit is done. 
+   def init(self, name):  
+     self.tempdata = []       
+     self.data = {} 
+     self.name = name 
+     print "Repo " + '"' + self.name + '"' + " is initialised" 	  
 	  
-   # Initialise the "repo" 	  
-   def init(self):
-	  self.data = {} 
-	  print "Repo is initialised" 	  
-	  
-   # Get the SHA1 hash of the data. Pth is the path 
-   # to the data. 
-   # Suppress the call to "self".  
-   # @staticmethod   	  
-   def sha1hash(self, pth): 
-	  f=open(pth,'rb').read()
-	  self.sh=hashlib.sha1(f).hexdigest() 	  
-	  self.dirname = self.sh[0:2]  
-	  print "dirname is " + self.dirname 
-	  print "sha1hash is " + self.sh  
-	  return self.sh, self.dirname  	  
-	  
-		  
-   # Add data to the repo. Give a label to the "commit". 		  
+   
+   # Add data to the repo. 
    # Note - For now, this does not actually store any data - only 
    # the hexdigest string.  
-
+   #  Add some data.   
+   def add(self, pth):  
+     f=open(pth,'rb').read()
+     self.sh=hashlib.sha1(f).hexdigest() 	  
+     self.dirname = self.sh[0:2]  
+     self.tempdata.append(self.sh) 
+     print "dirname is " + self.dirname 
+     print "sha1hash is " + self.sh  
+     print "Data added is " + str(self.tempdata) 
+     return self.sh, self.dirname 
+   
+	   	  		 	  
+   # Commit the data that has been added. 
+   # Give a label to the "commit". 		  
    def commit(self, mylabel):	
-	   self.data[self.dirname] = self.sh  
-	   self.label = mylabel 
+      self.label = mylabel 
+      for elem in self.tempdata: 
+	     self.data[self.dirname] = elem   
+   
 	  
    #  Print the contents of the "repo"  
    def display(self):
@@ -57,15 +61,13 @@ class repo():
 #  Test this cr^H^H code.... ;)  
 myclass = repo() 
 
-myclass.init() 
+myclass.init('my repo') 
 
-myclass.setname('test repo') 
-
-myclass.sha1hash('README')
-
-print myclass.name 
+myclass.add('README')
 
 myclass.commit("First commit") 
+
+
 
 myclass.display() 
 
